bfb9c78ef7912978ec2769557a7019a2fc6a2774
Caue Bruno de Almeida <RM30917@fiap.com.br>
2016-12-17 10:24:31 -0200

    Adding butter knife

--- app/src/main/java/com/example/rm30917/demowebservice/MainActivity.java
+++ app/src/main/java/com/example/rm30917/demowebservice/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.rm30917.demowebservice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.edtUsername);
+        password = (EditText) findViewById(R.id.edtPassword);
+    }
+
+    public void doLogin(View v) {
+        // TODO
+        String username = this.username.toString();
+        String password = this.password.toString();
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
 # Demo WebService
 
+- Async Task
+    - Background: **DO NOT** update view from background.
+    - EventBus or create an interface to modularize your code, about Activty w/ Async tasks too much together
+
+## Plus
+
+Some useful stuff.
+
+### Libraries
+
+- [Butter Knife](http://jakewharton.github.io/butterknife/) for bind on FindViewById, for example
